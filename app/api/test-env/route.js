37647ce42a72824f2c6b382591667d8dd6479531
Daniel Albert <RezSolutionServices@gmail.com>
2026-06-18 08:42:56 -0400

    test env

---
+++ app/api/test-env/route.js
@@ -0,0 +1,8 @@
+﻿import { NextResponse } from "next/server";
+
+export async function GET() {
+  return NextResponse.json({ 
+    passwordSet: !!process.env.ADMIN_PASSWORD,
+    length: process.env.ADMIN_PASSWORD?.length || 0
+  });
+}
